cf3e5ab70ec0e55cf5075cce7b9194ef3b9bb298
generativist <jbn@abreka.com>
2022-08-14 12:14:08 -0700

    lmao

--- example.go
+++ example.go
@@ -4,13 +4,14 @@
 package example
 
 import (
-	"context"
+    "context"
+    "fmt"
+    "github.com/coredns/coredns/core/dnsserver"
+    "github.com/coredns/coredns/plugin"
+    "github.com/coredns/coredns/plugin/metrics"
+    clog "github.com/coredns/coredns/plugin/pkg/log"
 
-	"github.com/coredns/coredns/plugin"
-	"github.com/coredns/coredns/plugin/metrics"
-	clog "github.com/coredns/coredns/plugin/pkg/log"
-
-	"github.com/miekg/dns"
+    "github.com/miekg/dns"
 )
 
 // Define log to be a logger with the plugin name in it. This way we can just use log.Info and
@@ -19,28 +20,36 @@ var log = clog.NewWithPlugin("example")
 
 // Example is an example plugin to show how to write a plugin.
 type Example struct {
-	Next plugin.Handler
+    Next plugin.Handler
 }
 
 // ServeDNS implements the plugin.Handler interface. This method gets called when example is used
 // in a Server.
 func (e Example) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
-	// This function could be simpler. I.e. just fmt.Println("example") here, but we want to show
-	// a slightly more complex example as to make this more interesting.
-	// Here we wrap the dns.ResponseWriter in a new ResponseWriter and call the next plugin, when the
-	// answer comes back, it will print "example".
+    // This function could be simpler. I.e. just fmt.Println("example") here, but we want to show
+    // a slightly more complex example as to make this more interesting.
+    // Here we wrap the dns.ResponseWriter in a new ResponseWriter and call the next plugin, when the
+    // answer comes back, it will print "example".
 
-	// Debug log that we've have seen the query. This will only be shown when the debug plugin is loaded.
-	log.Debug("Received response")
+    srvX := ctx.Value(dnsserver.Key{})
+    if srvX == nil {
+        fmt.Printf("null\n")
+    } else {
+        srv := srvX.(*dnsserver.Server)
+        fmt.Printf("%s:%s\n", srv.Addr, srv.Address())
+    }
 
-	// Wrap.
-	pw := NewResponsePrinter(w)
+    // Debug log that we've have seen the query. This will only be shown when the debug plugin is loaded.
+    log.Debug("Received response")
 
-	// Export metric with the server label set to the current server handling the request.
-	requestCount.WithLabelValues(metrics.WithServer(ctx)).Inc()
+    // Wrap.
+    pw := NewResponsePrinter(w)
 
-	// Call next plugin (if any).
-	return plugin.NextOrFailure(e.Name(), e.Next, ctx, pw, r)
+    // Export metric with the server label set to the current server handling the request.
+    requestCount.WithLabelValues(metrics.WithServer(ctx)).Inc()
+
+    // Call next plugin (if any).
+    return plugin.NextOrFailure(e.Name(), e.Next, ctx, pw, r)
 }
 
 // Name implements the Handler interface.
@@ -48,16 +57,16 @@ func (e Example) Name() string { return "example" }
 
 // ResponsePrinter wrap a dns.ResponseWriter and will write example to standard output when WriteMsg is called.
 type ResponsePrinter struct {
-	dns.ResponseWriter
+    dns.ResponseWriter
 }
 
 // NewResponsePrinter returns ResponseWriter.
 func NewResponsePrinter(w dns.ResponseWriter) *ResponsePrinter {
-	return &ResponsePrinter{ResponseWriter: w}
+    return &ResponsePrinter{ResponseWriter: w}
 }
 
 // WriteMsg calls the underlying ResponseWriter's WriteMsg method and prints "example" to standard output.
 func (r *ResponsePrinter) WriteMsg(res *dns.Msg) error {
-	log.Info("example")
-	return r.ResponseWriter.WriteMsg(res)
+    log.Info("example")
+    return r.ResponseWriter.WriteMsg(res)
 }
